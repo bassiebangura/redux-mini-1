ca41c01cf8c27f11c8b0eab1296e0ea604d13e6d
Bassie Bangura <bassiebangura@Bassies-MacBook-Pro.local>
2019-11-14 05:04:35 -0500

    Convert action types from string to variables

--- index.js
+++ index.js
@@ -27,14 +27,21 @@ function CreateStore(reducer) {
 	};
 }
 
-//Actual app code is as below
+
+// App Code
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
 function todos(state = [], action) {
 	switch (action.type) {
-		case 'ADD_TODO':
-			return [...state, action.todo];
-		case 'REMOVE_TODO':
+		case ADD_TODO:
+			return state.concat([ action.todo ]);
+		case REMOVE_TODO:
 			return state.filter((todo) => todo.id !== action.id);
-		case 'TOGGLE_TODO':
+		case TOGGLE_TODO:
 			return state.map(
 				(todo) => (todo.id !== action.id ? todo : Object.assign({}, todo, { complete: !todo.complete }))
 			);
@@ -45,9 +52,9 @@ function todos(state = [], action) {
 
 function goals(state = [], action) {
 	switch (action.type) {
-		case 'ADD_GOAL':
-			return [...state, action.goal];
-		case 'REMOVE_GOAL':
+		case ADD_GOAL:
+			return state.concat([ action.goal ]);
+		case REMOVE_GOAL:
 			return state.filter((goal) => goal.id !== action.id);
 		default:
 			return state;
@@ -61,15 +68,14 @@ function app(state = {}, action) {
 	};
 }
 
+const store = createStore(app);
 
 store.subscribe(() => {
 	console.log('The new state is: ', store.getState());
 });
 
-
-
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo: {
 		id: 0,
 		name: 'Walk the dog',
@@ -78,7 +84,7 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo: {
 		id: 1,
 		name: 'Wash the car',
@@ -87,7 +93,7 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'ADD_TODO',
+	type: ADD_TODO,
 	todo: {
 		id: 2,
 		name: 'Go to the gym',
@@ -96,17 +102,17 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'REMOVE_TODO',
+	type: REMOVE_TODO,
 	id: 1
 });
 
 store.dispatch({
-	type: 'TOGGLE_TODO',
+	type: TOGGLE_TODO,
 	id: 0
 });
 
 store.dispatch({
-	type: 'ADD_GOAL',
+	type: ADD_GOAL,
 	goal: {
 		id: 0,
 		name: 'Learn Redux'
@@ -114,7 +120,7 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'ADD_GOAL',
+	type: ADD_GOAL,
 	goal: {
 		id: 1,
 		name: 'Lose 20 pounds'
@@ -122,58 +128,6 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'REMOVE_GOAL',
+	type: REMOVE_GOAL,
 	id: 0
 });
-// //What actual developer code will look like
-// function todosReducer(state = [], action) {
-// 	switch (action.type) {
-// 		case "ADD_TODO":
-// 			return [...state, action.todo];
-// 		case "REMOVE_TODO":
-// 			return state.filter(todo => todo.id !== action.id);
-// 		case "TOGGLE_TODO":
-// 			return state.map(todo =>
-// 				todo.id !== action.id
-// 					? todo
-// 					: Object.assign({}, todo, { complete: !todo.complete })
-// 			);
-// 		default:
-// 			return state;
-// 	}
-// }
-
-// function goalsReducer(state = [], action) {
-//     switch (action.type) {
-//         case 'ADD_GOAL':
-//             return [...state, action.todo]
-//         case 'REMOVE_GOAL':
-//             return state.filter((goal) => goal.id !== action.id)
-//         default:
-//             return state
-//     }
-// }
-
-// function app(state = {}, action) {
-//     return {
-//         todos: todos(state.todos, action),
-//         goals: goals(state.goals, action),
-//     }
-// }
-
-// //Actual developer code creating store and passing reducer needed by library code.
-
-// const store = createStore(app)
-
-// store.subscribe(() => {
-//     console.log('The new state is: ', store.getState())
-// })
-
-// store.dispatch({
-//     type: 'ADD_TODO',
-//     todo: {
-//         id: 0,
-//         name: 'Learn Redux',
-//         complete: false
-//     }
-// })
